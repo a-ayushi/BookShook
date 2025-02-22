319795d29a11d6e4c522314445f3db70ef6b89e2
a-ayushi <108705988+a-ayushi@users.noreply.github.com>
2025-02-22 15:03:11 +0530

    Add files via upload

---
+++ horizontalscroll.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const scrollContainer = document.querySelector('.scroll-container');
+//     const scrollLeftBtn = document.querySelector('.scroll-left');
+//     const scrollRightBtn = document.querySelector('.scroll-right');
+
+//     scrollLeftBtn.addEventListener('click', () => {
+//         scrollContainer.scrollBy({ left: -200, behavior: 'smooth' });
+//     });
+
+//     scrollRightBtn.addEventListener('click', () => {
+//         scrollContainer.scrollBy({ left: 200, behavior: 'smooth' });
+//     });
+// });
